5ac295f5e41543c3dd21beae7f450f69b7206ac8
teenari <teenari@users.noreply.github.com>
2020-10-07 18:09:11 -0700

    sdsf

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+    $('.locker').children('div').hover((e) => {
+        const target = e.currentTarget;
+       target.children[0].children[0].style.width = '165px';
+       target.children[0].children[0].style.left = '-31px';
+       target.children[1].style.top = '118px';
+    }, (e) => {
+        const target = e.currentTarget;
+        target.children[0].children[0].style.width = '';
+        target.children[0].children[0].style.left = '';
+       target.children[1].style.top = '';
+    });
+});
