3835143183d70e1bb53fc4b0c5d76ea8354bb777
teenari <teenari@users.noreply.github.com>
2020-10-08 22:28:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,15 +24,12 @@ class Locker {
     setSkin(item) {
         const skin = $('#skin');
         skin.children()[0].children[0].src = item.images.icon;
-        skin.css('background-image', '');
         skin.children().eq(1).css('background', '');
         if(item.series && item.series.image) {
-            const image = item.series.image;
             const VectorParameterValues = item.series.VectorParameterValues;
             if(VectorParameterValues[0]) {
                 skin.children().eq(1).css('background', VectorParameterValues[0].Hex);
                 skin.css('background', `linear-gradient(350deg, rgba(${VectorParameterValues[1].R},${VectorParameterValues[1].G},${VectorParameterValues[1].B},${VectorParameterValues[1].A}) 0%, rgba(${VectorParameterValues[0].R},${VectorParameterValues[0].G},${VectorParameterValues[0].B},${VectorParameterValues[0].A}) 100%)`);
-                console.log(VectorParameterValues)
             }
         }
         return this;
